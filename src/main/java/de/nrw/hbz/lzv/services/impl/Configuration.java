2e4d497ea466eb20b74549abfa720cfe3f06d05b
Andres Quast <quast@hbz-nrw.de>
2025-03-17 18:32:32 +0100

    add hbzs new font

---
+++ src/main/java/de/nrw/hbz/lzv/services/impl/Configuration.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package de.nrw.hbz.lzv.services.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import de.nrw.hbz.lzv.services.configurationUtil.PropertiesLoader;
+
+/**
+ * 
+ */
+public class Configuration {
+
+  private PropertiesLoader propLoader = new PropertiesLoader();
+  
+  private Properties readLogProperties() throws IOException {
+    Properties logProps = new Properties();
+        InputStream propStream = this.getClass().getResourceAsStream("log4j.properties");
+        if (propStream == null) {
+             throw new IOException("failed to load log4j.properties: file not found");
+            }else{
+                logProps.load(propStream);
+                System.out.println("read log4j-configuration");
+            }
+        return logProps;       
+  }
+
+}
---
+++ src/main/java/de/nrw/hbz/lzv/services/plugin/callasPilot/serviceImpl/ServiceImpl.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package de.nrw.hbz.lzv.services.plugin.callasPilot.serviceImpl;
+
+/**
+ * 
+ */
+public class ServiceImpl {
+
+}
