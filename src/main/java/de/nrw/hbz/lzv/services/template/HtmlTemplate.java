b8cc23990ab4f481109bcbb901e45be71f46720b
Andres Quast <quast@hbz-nrw.de>
2025-03-18 18:04:19 +0100

    added page templates

--- src/main/java/de/nrw/hbz/lzv/services/template/HtmlTemplate.java
+++ src/main/java/de/nrw/hbz/lzv/services/template/HtmlTemplate.java
@@ -3,19 +3,32 @@
  */
 package de.nrw.hbz.lzv.services.template;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import de.nrw.hbz.lzv.services.plugin.veraPDF.template.VeraPDFTemplate;
+
 /**
  * 
  */
 public class HtmlTemplate {
 
+  private static Hashtable<String, ArrayList<String>> menu = new Hashtable<>();
+  private static VeraPDFTemplate vpt = new VeraPDFTemplate();
+  
   /**
    * @return a HTML-Document Head
    */
   public static String getHtmlHead() {
     StringBuffer headSb = new StringBuffer();
     headSb.append("<html>\n" + "<head>\n" + "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n"
-        + "<link rel=\"stylesheet\" href=\"css\" />\n" + "<title>hbz lzv services</title>\n" + "</head>\n<body>\n"
-        + "<div class=\"main\">");
+        + "<link rel=\"stylesheet\" href=\"css\" />\n" + "<title>hbz lzv services</title>\n" + "</head>\n<body>\n");
+    headSb.append("<div class=\"head\">");
+    
+    headSb.append(getMenuEntry("veraPDF"));
+    
+    headSb.append("</div></div><div class=\"main\"><hr/>");
 
     return headSb.toString();
   }
@@ -32,4 +45,25 @@ public class HtmlTemplate {
     return footSb.toString();
   }
 
+  public void appendMenu(Hashtable<String,ArrayList<String>> pluginMenu) {
+    menu.putAll(pluginMenu);
+  }
+  
+  private static String getMenuEntry(String entry) {
+    StringBuffer menuSb = new StringBuffer();
+    Enumeration<String> mEnum = menu.keys();
+    while(mEnum.hasMoreElements()) {
+      String key = mEnum.nextElement();
+      if(key.equals(entry)){
+         menuSb.append("<div class=\"dropdown\"><div class=\"menu\"><img class=\"menu\" src=\"https://docs.verapdf.org/favicon-32x32.png\" />"
+        + "veraPDF<div class=\"submenu\"><ul>");
+        ArrayList<String> subMenu = menu.get(key);
+        for(int i=0; i< subMenu.size(); i++) {
+          menuSb.append("<li>" + subMenu.get(i) + "</li>");
+        }
+        menuSb.append("</ul></div></div>");
+      }
+    }
+   return menuSb.toString(); 
+  }
 }
