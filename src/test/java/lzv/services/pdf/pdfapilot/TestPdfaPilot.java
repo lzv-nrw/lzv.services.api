c47378bb64f8c2f3a96c3e6e5e81e12235ae44dc
Kayhan Ogan <kayhan.ogan@hbz-nrw.de>
2026-08-12 12:02:05 +0200

    fix test

--- src/test/java/lzv/services/pdf/pdfapilot/TestPdfaPilot.java
+++ src/test/java/lzv/services/pdf/pdfapilot/TestPdfaPilot.java
@@ -3,7 +3,7 @@
  */
 package lzv.services.pdf.pdfapilot;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
@@ -28,17 +28,17 @@ public class TestPdfaPilot {
 
     TestPdfaPilot tpap = new TestPdfaPilot();
     tpap.getExecuteString();
-    
+
   }
-  
+
   @Test
   public void getExecuteString() {
-    
+
     PdfACreator pdfaPC = PdfACreator.getInstance("pdfapilot");
-    PdfaPilotResult result = pdfaPC.createPdfa(new File("src/test/resources/pdf.pdf"), "pdf.pdf" , "3a");
-    //logger.info(result);
-    //assertEquals("run for test only", result.getSummaryList().get(0).toString());
-    
+    PdfaPilotResult result = pdfaPC.createPdfa(new File("src/test/resources/pdf.pdf"), "pdf.pdf", "3a");
+    logger.info("TEST result " + result.getSummaryList().get(0).toString());
+    assertTrue(result.getSummaryList().get(0).toString().contains("run for test only"));
+
   }
 
 }
